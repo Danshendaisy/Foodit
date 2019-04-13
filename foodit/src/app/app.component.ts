4b08fd0e29b5f78760bb831492b0addcd66022a0
Danshendaisy <37489386+Danshendaisy@users.noreply.github.com>
2019-04-13 18:55:07 -0400

    frontend ui

--- foodit/src/app/app.component.ts
+++ foodit/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'FOODIT!';
+  title = 'FOODIT';
+  description = "Bid on your favorate food!"
 }
