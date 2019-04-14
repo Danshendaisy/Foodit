d846262892d3a18a9aada219624916e653b05c69
Danshendaisy <37489386+Danshendaisy@users.noreply.github.com>
2019-04-14 19:25:47 -0400

    fix and food api

--- foodit/src/app/in-memory-data.service.ts
+++ foodit/src/app/in-memory-data.service.ts
@@ -10,16 +10,16 @@ export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     
     const foodlist = [
-      { id: 11, name: 'Chiken', price:10, pic: '/assets/pork.jpg' },
-      { id: 12, name: 'Beef', price:12, pic:'assets/beef.jpg'},
-      { id: 13, name: 'Lamb' ,price:12, pic:'assets/lamb.jpg'},
-      { id: 14, name: 'Fish' ,price:12, pic:'assets/salmon.jpg'},
-      { id: 15, name: 'Tofu',price:12, pic:'assets/tofu.jpg' },
-      { id: 16, name: 'Pork',price:12, pic:'assets/pork.jpg' },
-      { id: 17, name: 'Egg',price:12, pic:'assets/egg.jpg' },
-      { id: 18, name: 'Vegetable',price:12, pic:'assets/vegetable.jpg' },
-      { id: 19, name: 'Fruit',price:12, pic:'assets/fruits.jpeg'  },
-      { id: 20, name: 'Beverage',price:12, pic:'assets/beverage.jpg' }
+      { id: 11, name: 'Chiken', price:10, image: 'assets/chicken.jpg' },
+      { id: 12, name: 'Beef', price:12, image:'assets/beef.jpg'},
+      { id: 13, name: 'Lamb' ,price:12, image:'assets/lamb.jpg'},
+      { id: 14, name: 'Fish' ,price:12, image:'assets/salmon.jpg'},
+      { id: 15, name: 'Tofu',price:12, image:'assets/tofu.jpg' },
+      { id: 16, name: 'Pork',price:12, image:'assets/pork.jpg' },
+      { id: 17, name: 'Egg',price:12, image:'assets/egg.jpg' },
+      { id: 18, name: 'Vegetable',price:12, image:'assets/vegetable.jpg' },
+      { id: 19, name: 'Fruit',price:12, image:'assets/fruits.jpeg'  },
+      { id: 20, name: 'Beverage',price:12, image:'assets/beverage.jpg' }
     ];
     const shoppingCart=[
       { id: 12, name: 'Beef', price:12, pic:null},
